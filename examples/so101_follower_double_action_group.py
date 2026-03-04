e105712b66471257ec61e899e737f5cd396b8238
ydy0615 <allenyuan410@gmail.com>
2026-03-04 22:38:07 +0800

    modified:   examples/so101_follower_double_action_group.py

--- examples/so101_follower_double_action_group.py
+++ examples/so101_follower_double_action_group.py
@@ -50,11 +50,56 @@ POSE_RAISE = [-8.6,-77.1,55.1,36.8,-17.2,5.6,6.3,-71.2,52.9,33.6,6.7,0.4]
 POSE_EXTEND = [-8.1,-8.5,-76.7,3.3,-17.6,63.3,5.7,-4.0,-83.0,-5.4,6.6,52.7]
 POSE_CLOSE = [-8.3,-100.0,98.6,77.9,-16.8,3.3,5.8,-99.6,96.8,74.7,7.0,1.0]
 
+raise_right1 = [-11.6,-1.5,-94.4,9.5,-17.1,4.3,5.9,-98.9,96.9,64.0,6.5,1.9]
+raise_right2 = [73.5,3.6,-86.3,-2.3,-8.4,4.0,5.7,-98.9,97.0,64.0,6.5,1.9]
+raise_right3 = [71.0,3.6,-86.3,-2.2,92.6,4.0,5.8,-98.9,97.0,64.0,6.5,1.9]
+
+raise_right4 = [71.0,4.7,-55.6,-1.5,92.6,4.0,5.7,-98.9,97.0,64.0,6.5,1.9]
+raise_right5 =  [71.0,-34.3,-87.6,-2.4,92.6,5.6,5.7,-98.9,97.0,64.0,6.5,1.9]
+raise_right6 =  [70.9,30.0,-86.4,-1.6,92.6,4.2,5.7,-98.9,97.0,64.0,6.5,1.9]
+
+
+raise_right7 = [71.1,-24.4,-87.6,-2.4,92.6,5.6,5.8,-98.9,97.0,64.0,6.5,1.9]
+raise_right8 = [71.0,2.5,-87.5,-2.4,92.6,4.5,5.7,-98.9,97.0,64.0,6.5,1.9]
+raise_right9 = [70.9,2.5,-86.6,20.0,92.5,100.0,5.8,-98.9,97.0,64.0,6.5,1.9]
+raise_right10 = [71.0,2.5,-87.4,0.5,92.5,8.4,5.8,-98.9,97.0,64.0,6.5,1.9]
+
+raise_all1 = [-8.3,-99.8,35.2,46.6,-17.1,4.8,5.9,-99.5,37.4,44.4,6.4,1.6]
+raise_all2 = [-15.5,-8.0,-76.7,2.0,-16.7,4.5,6.2,-13.5,-79.5,-2.4,6.5,1.6]
+raise_all3 = [80.5,-9.0,-77.6,1.5,-16.7,4.5,-78.9,-13.6,-79.8,-2.5,6.5,1.6]
+raise_all4 = [80.4,27.1,-76.7,2.2,-17.3,4.5,-79.1,37.6,-95.9,-1.7,6.4,1.6]
+raise_all5 =  [69.8,-28.4,-77.6,1.4,-16.7,4.5,-79.2,-15.4,-96.5,-2.5,6.5,1.6]
+raise_all6 =  [69.2,29.8,-85.5,2.1,-17.3,4.5,-79.2,30.3,-95.6,-1.8,6.5,1.6]
+raise_all7 =  [69.8,-24.9,-86.3,1.4,-16.7,4.5,-78.7,-15.5,-96.4,-2.5,6.5,1.6]
+raise_all8 =  [-10.2,-1.8,-85.5,2.0,-16.7,4.5,6.5,-3.8,-87.0,-2.5,6.6,1.6]
+
 TRAJECTORY = [
-    ("home", POSE_HOME, 1.0),
-    ("raise", POSE_RAISE, 2.0),
-    ("extend", POSE_EXTEND, 2.5),
-    ("return_home", POSE_HOME, 1.5),
+    ("home", POSE_HOME, 0.5),
+    ("raise_right", raise_right1, 3.0),
+    ("raise_right", raise_right2, 1.5),
+    ("raise_right", raise_right3, 1.5),
+    ("raise_right", raise_right4, 1.5),
+    ("raise_right", raise_right5, 1.5),
+    ("raise_right", raise_right6, 1.5),
+    ("raise_right", raise_right7, 1.0),
+    ("raise_right", raise_right8, 1.0),
+    ("raise_right", raise_right9, 1.0),
+    ("raise_right", raise_right10, 1.0),
+    ("raise_right", raise_right9, 1.0),
+    ("raise_right", raise_right10, 2.0),
+    ("return_home", POSE_HOME, 5.0),
+    ("raise_all", raise_all1, 3.0),
+    ("raise_all", raise_all2, 2.0),
+    ("raise_all", raise_all3, 1.5),
+    ("raise_all", raise_all4, 1.5),
+    ("raise_all", raise_all5, 1.5),
+    ("raise_all", raise_all6, 1.5),
+    ("raise_all", raise_all7, 1.5),
+    ("raise_all", raise_all8, 1.5),
+    ("return_home", POSE_HOME, 5.0),
+#    ("raise", POSE_RAISE, 5.0),
+#    ("extend", POSE_EXTEND, 8.0),
+#    ("return_home", POSE_HOME, 15),
 ]
 
 
@@ -136,6 +181,10 @@ def create_app(port: str, robot_id: str) -> FastAPI:
     def on_startup() -> None:
         print(f"[connect] port={port}, robot_id={robot_id}")
         state.robot.connect(calibrate=False)
+        for motor in state.robot.bus.motors:
+            state.robot.bus.write("Acceleration", motor, 50)
+            state.robot.bus.write("Maximum_Acceleration", motor, 80)
+            state.robot.bus.write("Goal_Velocity", motor, 640)
         state.robot.bus.disable_torque()
         state.last_result = "idle"
         print("[ready] robot connected and torque disabled.")
@@ -214,13 +263,16 @@ def main() -> int:
         return run_api_server(args)
 
     robot = create_robot(args.port, args.robot_id)
-
+    
     connected = False
     try:
         print("[connect] " f"port={args.port}, robot_id={args.robot_id}")
         robot.connect(calibrate=False)
         connected = True
-
+        for motor in robot.bus.motors:
+            robot.bus.write("Acceleration", motor, 50)
+            robot.bus.write("Maximum_Acceleration", motor, 80)
+            robot.bus.write("Goal_Velocity", motor, 640)
         run_trajectory(robot)
         print("[done] trajectory complete.")
         return 0
